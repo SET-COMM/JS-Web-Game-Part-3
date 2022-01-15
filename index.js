b152cec5a24fdfa0b2793aa6a574f9de73f117bc
SET-COMM <92279033+SET-COMM@users.noreply.github.com>
2022-01-15 12:30:22 -0800

    Exercise Web GAme Part III

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-function newImage(url, left, bottom){
+
+
+//position logic
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
+//move position of image
+function move(image){
+    image.style.position = 'fixed'
+    function moveToCoordinates(left,bottom){
+        
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    }
+    return {
+        to: moveToCoordinates
+    }
+}
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+//let greenCharacter= newImage('assets/green-character.gif')
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/tree.png')).to(200,450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+newItem('assets/sword.png', 500, 555)
+newItem('assets/shield.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
+//newImage('assets/green-character.gif', 100, 250)
+//newImage('assets/tree.png', 200, 450)
+//newImage('assets/pillar.png', 350, 250)
+//newImage('assets/pine-tree.png', 450, 350)
+//newImage('assets/crate.png', 150, 350)
+//newImage('assets/well.png', 500, 575)
 
 
 function newItem(url, left, bottom){
@@ -27,9 +49,7 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+
 
 function newInventory(){
     let inventory = document.createElement('div')
